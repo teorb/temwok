063a8c1dab5bcf2c598ffff4f75b2170b6ad3bbb
Teo Rivera <141681082+teorb@users.noreply.github.com>
2024-02-26 09:12:17 -0500

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,120 @@
+(function ($) {
+    "use strict";
+
+    //Barra de navegación al desplazarse
+    $(window).scroll(function () {
+        if ($(this).scrollTop() > 100) {
+            $('.navbar').css('display', 'flex');
+        } else {
+            $('.navbar').css('display', 'none');
+        }
+    });
+
+
+    // Desplazamiento suave en los enlaces de la barra de navegación
+    $(".navbar-nav a, .btn-scroll").on('click', function (event) {
+        if (this.hash !== "") {
+            event.preventDefault();
+            
+            $('html, body').animate({
+                scrollTop: $(this.hash).offset().top - 45
+            }, 1500, 'easeInOutExpo');
+            
+            if ($(this).parents('.navbar-nav').length) {
+                $('.navbar-nav .active').removeClass('active');
+                $(this).closest('a').addClass('active');
+            }
+        }
+    });
+
+
+    // Desplazarse hacia abajo
+    $(window).scroll(function () {
+        if ($(this).scrollTop() > 100) {
+            $('.scroll-to-bottom').fadeOut('slow');
+        } else {
+            $('.scroll-to-bottom').fadeIn('slow');
+        }
+    });
+
+
+    // Portfolio isotope and filter
+    var portfolioIsotope = $('.portfolio-container').isotope({
+        itemSelector: '.portfolio-item',
+        layoutMode: 'fitRows'
+    });
+    $('#portfolio-flters li').on('click', function () {
+        $("#portfolio-flters li").removeClass('active');
+        $(this).addClass('active');
+
+        portfolioIsotope.isotope({filter: $(this).data('filter')});
+    });
+    
+    
+    // Botón volver al principio
+    $(window).scroll(function () {
+        if ($(this).scrollTop() > 750) {
+            $('.back-to-top').fadeIn('slow');
+        } else {
+            $('.back-to-top').fadeOut('slow');
+        }
+    });
+    $('.back-to-top').click(function () {
+        $('html, body').animate({scrollTop: 0}, 1500, 'easeInOutExpo');
+        return false;
+    });
+
+
+    // Gallery carousel
+    $(".gallery-carousel").owlCarousel({
+        autoplay: true,
+        autoplaySpeed: 1500, // Tiempo entre transiciones en milisegundos
+        smartSpeed: 1500, // Velocidad de transición en milisegundos
+        dots: false,
+        loop: true,
+        nav: true,
+        navText: [
+            '<i class="fa fa-angle-left" aria-hidden="true"></i>',
+            '<i class="fa fa-angle-right" aria-hidden="true"></i>'
+        ],
+        responsive: {
+            0: {
+                items: 1
+            },
+            576: {
+                items: 2
+            },
+            768: {
+                items: 3
+            },
+            992: {
+                items: 4
+            },
+            1200: {
+                items: 5
+            }
+        }
+    });
+    
+    //ocultar barra de navegacion al dar clik en un enlace
+    $(document).on('click', 'a', function () {
+        $('.navbar-collapse').collapse('hide');
+    });
+
+
+    // Testimonials carousel
+    $(".testimonial-carousel").owlCarousel({
+        autoplay: true,
+        smartSpeed: 1500,
+        items: 1,
+        dots: false,
+        loop: true,
+        nav : true,
+        navText : [
+            '<i class="fa fa-angle-left" aria-hidden="true"></i>',
+            '<i class="fa fa-angle-right" aria-hidden="true"></i>'
+        ],
+    });
+    
+})(jQuery);
+
